5017fab74679e6c990223cb7ef9ad775ef8aa57f
Manoj Tiwari <manoj41@gmail.com>
2015-07-26 16:08:56 -0700

    Queue impl.

---
+++ binary_tree/queue.py
@@ -0,0 +1,57 @@
+class Queue():
+  def __init__(self, data):
+    self.front = None
+    self.back = None
+    self.data = data
+
+  # return new back of queue.
+  def put(self, data):
+    node = self
+    # Get to the back of queue.
+    while node.back != None:
+      node = node.back
+    node.back = Queue(data);
+    node.back.front = node;
+    return node.back
+
+  def get(self):
+    node = self
+    # get to the front
+    while self.front != None: 
+      self = self.front
+
+    self.back.front = None
+    return self.data
+
+  def walk(self):
+    node = self
+    # go to the back and start walking towards front
+    while node.back != None:
+      node = node.back
+
+    # now print and walk towards front
+    while node.front != None:
+      print node.data
+      node = node.front
+
+    print node.data
+
+
+
+def main():
+    queue = Queue(1)
+    queue = queue.put(2)
+    queue = queue.put(3)
+    queue = queue.put(4)
+  
+    print 'walk the queue'
+    queue.walk()
+    print 'front of queue'
+    print queue.get()
+    print 'front of queue'
+    print queue.get()
+    print 'walk the queue'
+    queue.walk()
+
+if __name__ == "__main__":
+    main()
